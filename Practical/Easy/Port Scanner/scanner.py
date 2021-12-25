a82128ba0233feb6b19be7b6c4a916480673ee71
saintwithataint <arishabh748@gmail.com>
2021-12-25 21:37:03 +0530

    Port scanner complete
    Signed-off-by: saintwithataint <arishabh748@gmail.com>

---
+++ Practical/Easy/Port Scanner/scanner.py
@@ -0,0 +1,24 @@
+import socket
+from datetime import date, datetime
+
+target = socket.gethostbyname("www.duckduckgo.com")
+print("Scanning started at:", str(datetime.now()))
+t1 = datetime.now()
+try:
+    for port in range(1, 65536):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print("Port {} is open".format(port))
+        s.close()
+    print("Time Taken:", datetime.now() - t1)
+except KeyboardInterrupt:
+    print("Lol peace out")
+    print("Time Taken:", datetime.now() - t1)
+except socket.gaierror:
+    print("Hostname not resolved")
+    print("Time Taken:", datetime.now() - t1)
+except socket.error:
+    print("Unresponsive server")
+    print("Time Taken:", datetime.now() - t1)
